8dec93e790442d72f545df6d0ab6d6997baf3b48
刘金发 <liujinfa9242@dingtalk.com>
2024-05-11 17:23:52 +0800

    feat(log): 代码规范整理

--- oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/analysis/manual/log/LogStrUtil.java
+++ oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/analysis/manual/log/LogStrUtil.java
@@ -2,50 +2,14 @@ package org.apache.skywalking.oap.server.core.analysis.manual.log;
 
 import java.util.ArrayList;
 import java.util.Map;
-import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-/**
- * @author fa
- */
 public class LogStrUtil {
 
     /**
-     * 解析日志内容
-     */
-    public static Optional<Map.Entry<String, String>> parseBy(String content, String key) {
-        String pattern = "\\#\\@\\[[^\\]]*\\]";
-        Pattern regex = Pattern.compile(pattern);
-        Matcher matcher = regex.matcher(content);
-        ArrayList<String> objects = new ArrayList<>();
-        while (matcher.find()) {
-            objects.add(unWrap(matcher.group(), "#@[", "]"));
-        }
-        return objects.stream().filter(s -> s.startsWith(key)).findFirst().map(s -> s.replaceFirst(key, "")).map(
-                s -> s.substring(1, s.length() - 1)
-        ).map(s -> Map.entry(key, s));
-    }
-
-    /**
-     * 解析日志内容
-     */
-    public static Map<String, String> parseAllBy(String content, String prefix, String suffix) {
-        String pattern = "\\#\\@\\[[^\\]]*\\]";
-        Pattern regex = Pattern.compile(pattern);
-        Matcher matcher = regex.matcher(content);
-        ArrayList<String> objects = new ArrayList<>();
-        while (matcher.find()) {
-            objects.add(unWrap(matcher.group(), "#@[", "]"));
-        }
-        return objects.stream().map(s -> s.split("=")).collect(Collectors.toMap(s -> s[0], s -> s[1], (v1, v2) -> v2));
-    }
-
-    /**
-     * 解析日志内容
+     * Parsing log content
      */
     public static Map<String, String> parseAllBy(String content) {
         String pattern = "\\#\\[[^\\]]*\\]";
